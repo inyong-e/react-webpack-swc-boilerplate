fff7c595c3194b595044cea8d9550cd619deb014
inyong-e <jiy8319@gmail.com>
2023-01-24 17:56:53 +0900

    feat: 우선 대비용 바벨 세팅

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,36 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.jsx',
   output: {
     path: path.join(__dirname, '/dist'),            //__dirname : 현재 디렉토리, dist 폴더에 모든 컴파일된 하나의 번들파일을 넣을 예정
     filename: 'index_bundle.js'
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
     compress: true,
     port: 3001
   },
   module: {
     rules: [
       {
-        test: /\.m?js$/,
-        exclude: /(node_modules)/,
+        test: /\.(js|jsx)$/,
+        exclude: /node_module/, // node module 폴더는 babel 컴파일에서 제외
         use: {
-          loader: "swc-loader"
+          loader: 'babel-loader',
+          options:{
+            presets: [
+              "@babel/preset-env", ["@babel/preset-react", {"runtime": "automatic"}]
+            ]
+          }          
         }
-      }
+      },
     ]
   },
   plugins: [
-    new HTMLWebpackPlugin({
+    new HtmlWebpackPlugin({
       filename: "./index.html",
       template: path.join(__dirname, 'public/index.html')
     })
